57c7014a07edf3a8ba2f1d902a465b324b1e81e6
catapasr <csalinas@trainee.cleveritgroup.com>
2023-02-07 18:28:13 -0300

    agregué header y shoorty como primeros componentes, aun estan incompletos

---
+++ components/Shoorty.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Shoorty() {
+  return (
+    <div className="container mx-auto my-20 bg-red-500 p-5 shadow ">
+      <h1>ShoortyApp</h1>
+    </div>
+  );
+}
